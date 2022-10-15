165b74b3fb6640934093f3fdd70adcc474a3ebad
doggo4242 <doggo4242@github.com>
2022-10-14 21:47:06 -0400

    fix middleware

--- blueprints/public/__init__.py
+++ blueprints/public/__init__.py
@@ -9,8 +9,7 @@ group = sanic.Blueprint.group(account.bp)
 
 @group.on_request
 async def check_login(request):
-	if 'session_token' in request.cookies and \
-			(await request.app.ctx.db['sessions'].find_one({'token': request.cookies['session_token']})) is not None:
-		return sanic.response.redirect('/')
-
-	del request.cookies['session_token']
+	if 'session_token' in request.cookies:
+		if (await request.app.ctx.db['sessions'].find_one({'token': request.cookies['session_token']})) is not None:
+			return sanic.response.redirect('/')
+		del request.cookies['session_token']
