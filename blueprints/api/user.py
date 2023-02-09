b1bbcd26283365c98aa7ee7fc079173b92216675
doggo4242 <doggo4242@protonmail.com>
2023-02-08 21:43:49 -0500

    speed up test function

--- blueprints/api/user.py
+++ blueprints/api/user.py
@@ -1,8 +1,37 @@
+import asyncio
+from typing import Union
+
+import aiohttp
 import sanic
 import statistics
 import aiofiles
 
 bp = sanic.Blueprint('user-api')
+
+
+async def test_solution(session: aiohttp.ClientSession, level: int, test_no: int, lang_id: str, code: str,
+						compile_args: str) -> tuple[bool, Union[str, int]]:
+	async with aiofiles.open(f'levels/{level}/{test_no}.in', 'r') as f:
+		test_input = await f.read()
+	async with session.post(
+			f'https://godbolt.org/api/compiler/{lang_id}/compile',
+			json={'source': code, 'options': {
+				'userArguments': compile_args,
+				'executeParameters': {
+					'stdin': test_input}, 'compilerOptions': {'executorRequest': True},
+				'filters': {'execute': True}}},
+			headers={'Accept': 'application/json'}) as res:
+		result = await res.json()
+	if result['buildResult']['code'] != 0:
+		return False, '\n'.join(map(lambda x: x['text'], result['stderr']))
+
+	output = '\n'.join(map(lambda x: x['text'], result['stdout'])).strip()
+	async with aiofiles.open(f'levels/{level}/{test_no}.out', 'r') as f:
+		ans = (await f.read()).strip()
+	if output != ans:
+		return False, 'Test failed.'
+
+	return True, int(result['execTime'])
 
 
 @bp.post('/levels/<level:int>')
@@ -23,28 +52,19 @@ async def run_test(request: sanic.Request, level: int):
 	except UnicodeDecodeError:
 		return sanic.response.text('Invalid file', status=400)
 
-	exec_times = []
-	for i in range(1, level_info['n_tests'] + 1):
-		async with aiofiles.open(f'levels/{level}/{i}.in', 'r') as f:
-			test_input = await f.read()
-		async with request.app.ctx.session.post(
-				f'https://godbolt.org/api/compiler/{lang_id}/compile',
-				json={'source': file, 'options': {
-					'userArguments': request.app.ctx.compile_args.get(lang, ''),
-					'executeParameters': {
-						'stdin': test_input}, 'compilerOptions': {'executorRequest': True},
-					'filters': {'execute': True}}},
-				headers={'Accept': 'application/json'}) as res:
-			result = await res.json()
-		if result['buildResult']['code'] != 0:
-			return sanic.response.text('\n'.join(map(lambda x: x['text'], result['stderr'])), status=400)
+	compile_args = request.app.ctx.compile_args.get(lang, '')
 
-		output = '\n'.join(map(lambda x: x['text'], result['stdout'])).strip()
-		async with aiofiles.open(f'levels/{level}/{i}.out', 'r') as f:
-			ans = (await f.read()).strip()
-		if output != ans:
-			return sanic.response.text('Test failed', status=400)
-		exec_times.append(int(result['execTime']))
+	exec_times = []
+	results = [test_solution(request.app.ctx.session, level, i, lang_id, file, compile_args) for i in
+			   range(1, level_info['n_tests'] + 1)]
+	for coro in asyncio.as_completed(results):
+		success, output = await coro
+		if success:
+			exec_times.append(output)
+		else:
+			for closed_coro in results:
+				closed_coro.close()
+			return sanic.response.text(output, status=400)
 
 	mean = statistics.mean(exec_times)
 	median = statistics.median(exec_times)
